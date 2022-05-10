4d8b7151ffcb63445bae2081a041e9bcc0aa78a6
Mattia D'Antonio <m.dantonio81@gmail.com>
2022-05-10 11:52:50 +0200

    # Tests fix

--- projects/seadata/backend/endpoints/pids.py
+++ projects/seadata/backend/endpoints/pids.py
@@ -13,8 +13,14 @@ if True:
 
     warnings.filterwarnings(
         "ignore",
-        # raised when imprting b2handle
+        # raised when imprting b2handle on python 3.10
         message="the imp module is deprecated in favour of importlib and slated for removal in Python 3.12; see the module's documentation for alternative uses",
+    )
+
+    warnings.filterwarnings(
+        "ignore",
+        # raised when imprting b2handle on python 3.9
+        message="the imp module is deprecated in favour of importlib; see the module's documentation for alternative uses",
     )
 
 from restapi import decorators
