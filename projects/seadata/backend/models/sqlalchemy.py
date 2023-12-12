24669eafb828a048e025f1fb85d6dafd8514163a
bchiavarini <b.chiavarini@cineca.it>
2023-12-12 10:31:24 +0100

    extended characters for path column in db

--- projects/seadata/backend/models/sqlalchemy.py
+++ projects/seadata/backend/models/sqlalchemy.py
@@ -7,7 +7,7 @@ from sqlalchemy.dialects.postgresql import JSONB
 class DataObject(db.Model):  # type: ignore
     id = db.Column(db.Integer, primary_key=True)
     uid = db.Column(db.String(46), unique=True, nullable=False)
-    path = db.Column(db.String(100), index=True, nullable=False)
+    path = db.Column(db.String(400), index=True, nullable=False)
     object_metadata = db.Column(JSONB, nullable=True)
 
     def __repr__(self):
